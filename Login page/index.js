1f294b33b82cab7886a444b989d031c1979cf6f1
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-02-22 11:38:09 +0900

    Add files via upload

---
+++ Login page/index.js
@@ -0,0 +1,13 @@
+function loginpage(){
+    return(
+        <h1>loginpage</h1>
+    )
+}
+const Welcome=styled.h1`
+font-size:100px
+color:blue
+margin-top:20vh
+background-color:red`;
+
+
+export default loginpage;
