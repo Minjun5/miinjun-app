7db4064c1816655f3289b44203c4c3bb00a5bd6f
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-03-08 10:34:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,5 +4,27 @@ import loginpage from './Login page';
 
 function App() {
   return <loginpage/>}
-
+import { div } from "prelude-ls";
+import { BrowseRouter as Router, Route } from "react-router-dom"
+import {useHistory}from "react-router-dom";
+import Loginpage from "./Loginpage";
+import MainPage from "./MainPage"
+const navigate=()=>{alert("click")}
+function Loginpage(){
+  const history=useHistory()
+}
+function App(){
+    return (
+        <div>
+            
+            <Router>
+                <Route exact path="/" element={<Loginpage/>}/>
+                <Route exact path="/main" component={<MainPage/>}/>
+            </Router>  
+            <Loginpage onclick={navigate}></Loginpage> 
+            
+        </div>
+    )
+}
+export default App
 export default App;
