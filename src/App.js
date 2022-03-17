e982148bf87760676e610f833d2b73766defeaf7
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-03-17 09:20:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function app() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-import { div } from "prelude-ls";
 import { BrowseRouter as Router, Route } from "react-router-dom"
-import {useHistory}from "react-router-dom";
 import Loginpage from "./Loginpage";
 import MainPage from "./MainPage"
-const navigate=()=>{alert("click")}
 
 
-function pp(){
+
+function App(){
     return (
         <div>
             
@@ -36,10 +12,9 @@ function pp(){
                 <Route exact path="/" element={<Loginpage/>}/>
                 <Route exact path="/main" component={<MainPage/>}/>
             </Router>  
-            <Loginpage onclick={navigate}></Loginpage> 
+            
             
         </div>
     )
 }
-}
-export default pp;
+export default App;
