6b87b1c2a2a2a16e73f55df0803684b78bcf6f03
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-03-29 01:49:03 +0900

    Update main.js

--- src/Mainpage/main.js
+++ src/Mainpage/main.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
-import { BrowseRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-const navigate=()=>{alert("click")}
+
 
 const B=styled.h1`image:url(https://www.howtogeek.com/wp-content/uploads/2019/03/gmail-1.png?width=1198&trim=1,1&bg-color=000&pad=1,1)
 width:10%
@@ -14,10 +14,12 @@ height:10%
 display:flex
 `
 
-const c=styled.h1`image:url(https://play-lh.googleusercontent.com/8_0SDfkFXAFm12A7XEqkyChCdGC055J6fC8JR7qynNuO3qNOczIoNHo4U4lad8xYMJOL)
+const C=styled.h1`image:url(https://play-lh.googleusercontent.com/8_0SDfkFXAFm12A7XEqkyChCdGC055J6fC8JR7qynNuO3qNOczIoNHo4U4lad8xYMJOL)
 width:10%
 height:10%
 display:flex`
+
+
 
 
 
@@ -26,12 +28,13 @@ function mainpage(){
     return (
         <div>
             <Router>
-                <Route exact path="/login" element={<Loginpage/>}/>
-                <Route exact path="/main" component={<MainPage/>}/>
+                <Route exact path="/login" element={<login/>}/>
+                <Route exact path="/main" component={<main/>}/>
             </Router>  
-            <Loginpage onclick={navigate}></Loginpage> 
+            
             <A></A>
             <B></B>
+            <C></C>
             
         </div>
     )
