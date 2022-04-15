f73d6def2f8169895c9cf1270a12b16f8977aa3a
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-04-16 03:11:42 +0900

    Update main.js

--- src/Mainpage/main.js
+++ src/Mainpage/main.js
@@ -1,28 +1,7 @@
 import styled from "styled-components"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import { useHistory } from "react-router-dom";
-
-
-const B=styled.h1`image:url(https://www.howtogeek.com/wp-content/uploads/2019/03/gmail-1.png?width=1198&trim=1,1&bg-color=000&pad=1,1)
-width:10%
-height:10%
-display:flex
-`
-const A=styled.h1`image:url(https://play-lh.googleusercontent.com/8sc6LSo3dRf54GaLdQR8UZfzd_fgHgWMJlNxGLP1HWPEU7YY4UxkyHc8-qCNwtyiqO55)
-width:10%
-height:10%
-display:flex
-`
-
-const C=styled.h1`image:url(https://play-lh.googleusercontent.com/8_0SDfkFXAFm12A7XEqkyChCdGC055J6fC8JR7qynNuO3qNOczIoNHo4U4lad8xYMJOL)
-width:10%
-height:10%
-display:flex`
-
-const Learn=styled.p`width:10%
-height:10%
-`
-
+import {useHistory} from "react-router-dom";
+import "./style2.css"
+const Learn=styled.button`background-color:red`
 
 
 
@@ -33,16 +12,19 @@ const history = useHistory()
     }
     return (
         <div>
-            <Router>
-                <Route exact path="/login" element={<login/>}/>
-                <Route exact path="/main" component={<main/>}/>
-            </Router>  
             
-            <A></A>
-            <B></B>
-            <C></C>
-            <Learn onclick={learnpage}>learn more</Learn>
+            <a href="https://mail.google.com/ ">   
+            <img src="https://www.howtogeek.com/wp-content/uploads/2019/03/gmail-1.png?width=1198&trim=1,1&bg-color=000&pad=1,1"/>
+            </a> 
             
+            <a href="https://twitter.com/">
+            <img src="https://play-lh.googleusercontent.com/8sc6LSo3dRf54GaLdQR8UZfzd_fgHgWMJlNxGLP1HWPEU7YY4UxkyHc8-qCNwtyiqO55"/>
+            </a>
+            <a href="https://www.kakaocorp.com/">
+            <img src="https://play-lh.googleusercontent.com/8_0SDfkFXAFm12A7XEqkyChCdGC055J6fC8JR7qynNuO3qNOczIoNHo4U4lad8xYMJOL"/>
+            </a>
+            
+            <Learn onclick={learnpage} className="A">learn more</Learn>
         </div>
     )
 }
